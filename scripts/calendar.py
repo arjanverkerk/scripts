86d7393905651cfe3889c6390b29b563d8145f67
Arjan Verkerk <arjan.verkerk@gmail.com>
2013-05-28 22:52:04 +0200

    Calendar working for music schedule of second half of this year.

--- scripts/calendar.py
+++ scripts/calendar.py
@@ -8,6 +8,17 @@ from __future__ import division
 
 import argparse
 import csv
+import datetime
+
+"""
+Example
+-------
+Subject,Start Date,Start Time,End Date,End Time,All Day
+Event,Description,Location,Private
+Final Exam,05/12/08,07:10:00 PM,05/12/08,10:00:00 PM,False,Two
+essay questions that will cover topics covered throughout the
+semester,"Columbia, Schermerhorn 614",True
+"""
 
 
 def get_parser():
@@ -22,11 +33,63 @@ def get_parser():
 
 def command(sourcepath, targetpath):
     """ Do something spectacular. """
-    with open(sourcepath) as sourcefile:
-        sourcereader = csv.DictReader(sourcefile)
-        for sourcerecord in sourcereader:
-            print(sourcerecord)
+    sourcefile = open(sourcepath)
+    reader = csv.DictReader(sourcefile)
 
+    targetfile = open(targetpath, 'w')
+    writer = csv.DictWriter(targetfile, fieldnames=[
+        'Subject',
+        'Start Date',
+        'Start Time',
+        'End Date',
+        'End Time',
+        'All Day Event',
+        'Description',
+        'Location',
+        'Private',
+    ])
+    writer.writeheader()
+
+    # Here it all happens
+    date = None  # Pyflakes.
+    for stringrecord in reader:
+        record = {k: unicode(v, 'utf-8') for k, v in stringrecord.items()}
+
+        # Take date from previous record if this one is empty
+        current_record_date = record['datum'].replace('.', '-').strip()
+        date = current_record_date if current_record_date else date
+        time = record['tijd'].replace('.', ':').strip()
+        dt1 = datetime.datetime.strptime(
+            ' '.join([date, time]), '%d-%m %H:%M',
+        ).replace(year=2013)
+        dt2 = dt1 + datetime.timedelta(minutes=90)
+
+        # Base data
+        result = {
+            'Start Date': dt1.strftime('%d/%m/%Y'),
+            'Start Time': dt1.strftime('%I:%M:%S %p'),
+            'End Date': dt2.strftime('%d/%m/%Y'),
+            'End Time': dt2.strftime('%I:%M:%S %p'),
+        }
+
+        # Specific data
+        organ = record['Organist'].strip()
+        if organ:
+            result.update({'Subject': 'Orgel {}'.format(organ)})
+            writer.writerow({k: v.encode('utf-8') for k, v in result.items()})
+
+        piano = record['Pianist'].strip()
+        if piano:
+            result.update({'Subject': 'Piano {}'.format(piano)})
+            writer.writerow({k: v.encode('utf-8') for k, v in result.items()})
+
+        team = record['Muz. Team'].replace('MT', '').strip()
+        if team:
+            result.update({'Subject': 'Muziekteam {}'.format(team)})
+            writer.writerow({k: v.encode('utf-8') for k, v in result.items()})
+
+    sourcefile.close()
+    targetfile.close()
 
 
 def main():
