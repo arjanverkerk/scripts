329a9920efea44299d23b1d7bbe9c4aec4bb4fe8
Arjan Verkerk <arjan.verkerk@gmail.com>
2014-01-18 12:49:08 +0100

    Work in progress for the media browser.

--- scripts/media/server.py
+++ scripts/media/server.py
@@ -9,6 +9,7 @@ import flask
 
 import argparse
 import logging
+import os
 import sys
 
 logger = logging.getLogger(__name__)
@@ -20,7 +21,13 @@ MIME = {
     '.ogv': 'video/ogg',
 }
 
-files = None
+RANGE_PATTERN = re.compile(
+    '^bytes=(?P<start>[0-9]+)-(?P<stop>[0-9]*)'
+)
+
+RANGE = 'bytes {start}-{stop}/{size}'
+
+paths = None
 
 
 def get_parser():
@@ -34,9 +41,9 @@ def get_parser():
 
 @app.route('/')
 def home():
-    """ View first path. """
-    location = files[0]
-    return flask.redirect(location)
+    """ Redirect to first media path. """
+    location = paths[0]
+    return flask.redirect(flask.url_for('media', path=location))
 
 
 @app.route('/media/<path:path>')
@@ -54,9 +61,10 @@ def media(path):
 @app.route('/image/<path:path>')
 def image(path):
     """ Return image html. """
+    src = '/image/{}'.format(paths.index(path))
     return flask.render_template(
         'media/image.html',
-        path=path,
+        src=src,
     )
 
 
@@ -68,11 +76,42 @@ def video(path):
         path=path,
     )
 
+@app.route('/data/<int:index>')
+def data(index):
+    """ Return data for a path, supporting partial content. """
+        path = paths[index]
+        size = os.stat(path).st_size
+        match = re.match(config.RANGE_PATTERN,
+                         flask.request.headers.get('range', ''))
+
+        if match:
+            start = int(match.group('start'))
+            try:
+                stop = int(match.group('stop'))
+            except ValueError:
+                stop = size - 1
+        else:
+            start = 0
+            stop = size - 1
+
+        response_headers = {
+            'content-type': utils.get_mime(path),
+            'content-range': config.RANGE.format(start=start,
+                                                 stop=size - 1, size=size),
+            'accept-ranges': 'bytes',
+        }
+
+        status = 200 if start == 0 else 206  # 200:OK or 206:Partial content
+
+        with open(path, 'rb') as f:
+            f.seek(start)
+            return f.read(start - stop), status, response_headers
+
 
 def command(file_paths):
     """ Do something spectacular. """
-    global files
-    files = file_paths
+    global paths
+    paths = file_paths
     app.run(host='0.0.0.0')
 
 
--- scripts/media/static/media/media.js
+++ scripts/media/static/media/media.js
@@ -1,8 +1,22 @@
+var mime = {
+  "jpg": "image",
+  "ogv": "video"
+}
+
 $(document).keydown(function(eventObject) {
   console.log(eventObject.which);
 });
 
-$(document).ready()
-$.get( "media/" + path, function( data ) {
-  $( "#media" ).html(data);
-});
+function update() {
+  var loc = window.location.pathname
+  var ext = loc.match(/[^\.]*$/)[0]
+  console.log(ext)
+  var url = loc.replace("media", mime[ext.toLowerCase()])
+  console.log(url)
+  $.get(url, function(data) {
+    $("#media").html(data);
+  });
+  
+}
+
+$(document).ready(update)
