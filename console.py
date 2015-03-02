56166cea7e94681807ac0cf87d724b102e3047b5
Arjan Verkerk <arjan.verkerk@gmail.com>
2015-03-02 21:36:43 +0100

    New scripts and add templates.

--- console.py
+++ console.py
@@ -8,22 +8,24 @@ from __future__ import division
 # These are in a separate module so that symlinks can be made automatically.
 scripts = [
     # gis
-    'gis-bbox       = scripts.gis.bbox:main',
-    'gis-copy-proj  = scripts.gis.copy_proj:main',
-    'gis-copy-ndv   = scripts.gis.copy_ndv:main',
-    'gis-set-srs    = scripts.gis.set_srs:main',
-    'gis-polygon    = scripts.gis.polygon:main',
-    'gis-sum        = scripts.gis.sum:main',
+    'gis-bbox      = scripts.gis.bbox:main',
+    'gis-copy-proj = scripts.gis.copy_proj:main',
+    'gis-copy-ndv  = scripts.gis.copy_ndv:main',
+    'gis-set-srs   = scripts.gis.set_srs:main',
+    'gis-polygon   = scripts.gis.polygon:main',
+    'gis-sum       = scripts.gis.sum:main',
     # gallery
-    'gallery-media  = scripts.gallery.media:main',
-    'gallery-meta = scripts.gallery.meta:main',
-    'gallery-html   = scripts.gallery.html:main',
+    'gallery       = scripts.gallery:main',
+    'gallery-html  = scripts.gallery.html:main',
+    'gallery-meta  = scripts.gallery.meta:main',
+    'gallery-index = scripts.gallery.index:main',
+    'gallery-media = scripts.gallery.media:main',
     # various
-    'dbf2csv        = scripts.dbf2csv:main',
-    'garmin-graph   = scripts.garmin:main',
-    'gcalendar      = scripts.calendar:main',
-    'ln_scripts     = scripts.ln_scripts:main',
-    'newpy          = scripts.newpy:main',
+    'dbf2csv       = scripts.dbf2csv:main',
+    'garmin-graph  = scripts.garmin:main',
+    'gcalendar     = scripts.calendar:main',
+    'ln_scripts    = scripts.ln_scripts:main',
+    'newpy         = scripts.newpy:main',
 ]
 
 # Some extra commands to symlink
