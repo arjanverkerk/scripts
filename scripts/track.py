5007b7db2a37eab2ae09c67f20acc6fa4d012858
Arjan Verkerk <arjan.verkerk@gmail.com>
2019-02-27 15:23:47 +0100

    Adding keys.

--- scripts/track.py
+++ scripts/track.py
@@ -2,38 +2,56 @@
 """
 Timetracker using curses.
 """
-from curses import curs_set, echo, wrapper
+from curses import curs_set, echo, noecho, wrapper
 from datetime import datetime as Datetime
 
-import time
 import argparse
 
 """
-Add key
-Remove key
-Select key
+Remove keys
+Select keys
 """
 
-def track(window, **kwargs):
-    # no cursor
-    curs_set(0)
-    
-    # echo on, because wrapper disables it
+
+def clock(window):
+    window.addstr(0, 0, str(Datetime.now().strftime('%H:%S')))
+
+
+def activate(window):
+    window.timeout(-1)
+    curs_set(2)
     echo()
 
-    # window.clear()
-    # window.refresh()
 
-
+def deactivate(window):
+    curs_set(0)
     window.timeout(1000)
-    window.addstr(5, 5, 'test')
-    window.addstr(6, 5, 'test two')
+    noecho()
+
+
+def track(window, **kwargs):
+    deactivate(window)
+    keys = set()
+
+    # header
+    window.addstr(3, 0, 'allowed keys')
+    window.addstr(4, 0, '------------')
 
     while True:
+        clock(window)
         c = window.getch(10, 10)
         if c == ord('q'):
             break
-        window.addstr(7, 5, str(Datetime.now()))
+        if c == ord('a'):
+            msg = 'Enter new key: '
+            window.addstr(1, 0, msg)
+            activate(window)
+            k = (window.getstr(1, len(msg), 16)).decode('utf-8')
+            deactivate(window)
+            window.addstr(1, 0, ' ' * (len(msg) + len(k)))
+            keys.add(k)
+            for i, k in enumerate(keys):
+                window.addstr(5 + i, 0, '[ ] %d %s' % (i, k))
 
 
 def get_parser():
