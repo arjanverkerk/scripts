89fb63af97a974d100b9ad491506aad50d0c8101
Arjan Verkerk <arjan.verkerk@gmail.com>
2019-02-27 21:57:58 +0100

    Subwindows and clock class.

--- scripts/track.py
+++ scripts/track.py
@@ -6,6 +6,7 @@ from curses import curs_set, echo, noecho, wrapper
 from datetime import datetime as Datetime
 
 import argparse
+import threading
 
 """
 Update the clocks in a thread
@@ -20,8 +21,18 @@ Update loop updating clock and active key
 """
 
 
-def clock(window):
-    window.addstr(0, 0, str(Datetime.now().strftime('%H:%S')))
+
+
+class Clock(object):
+    def __init__(self, window):
+        self.window = window.subwin(3, 10, 1, 1)
+        # self.window=window
+        self.window.border()
+        self.update()
+
+    def update(self):
+        self.window.addstr(1, 1, str(Datetime.now().strftime('%H:%M:%S')))
+        self.window.refresh()
 
 
 def activate(window):
@@ -37,28 +48,31 @@ def deactivate(window):
 
 
 def track(window, **kwargs):
+    window.border()
     deactivate(window)
     keys = set()
 
     # header
-    window.addstr(3, 0, 'allowed keys')
-    window.addstr(4, 0, '------------')
+    window.addstr(5, 1, 'allowed keys')
+    window.addstr(6, 1, '------------')
+
+    clock = Clock(window)
 
     while True:
-        clock(window)
+        clock.update()
         c = window.getch(10, 10)
         if c == ord('q'):
             break
         if c == ord('a'):
             msg = 'Enter new key: '
-            window.addstr(1, 0, msg)
+            window.addstr(4, 1, msg)
             activate(window)
-            k = (window.getstr(1, len(msg), 16)).decode('utf-8')
+            k = (window.getstr(4, len(msg), 16)).decode('utf-8')
             deactivate(window)
-            window.addstr(1, 0, ' ' * (len(msg) + len(k)))
+            window.addstr(4, 1, ' ' * (len(msg) + len(k)))
             keys.add(k)
             for i, k in enumerate(keys):
-                window.addstr(5 + i, 0, '[ ] %d %s' % (i, k))
+                window.addstr(7 + i, 1, '[ ] %d %s' % (i, k))
 
 
 def get_parser():
